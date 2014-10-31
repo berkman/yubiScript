79705a9162a26a3117cd21c112c1ced58da136a8
Mike Berkman <mike@berkman.co>
2014-10-31 10:09:37 -0400

    Cleanup

--- yubiScript.sh
+++ yubiScript.sh
@@ -6,23 +6,23 @@
 # - https://developers.yubico.com/yubico-piv-tool/YubiKey-NEO-PIV-Introduction.html
 #################################################
 
-# Add the yubico-piv-tool binary to your path
+#1)	Add the yubico-piv-tool binary to your path
 YUBI_BIN=$PWD/bin/
 export PATH=$PATH:${YUBI_BIN}
 
-#1)	Check for the required binaries
+#2)	Check for the required binaries
 hash openssl 2>/dev/null || { echo >&2 "I require openssl but it's not installed.  Aborting."; exit 1; }
 hash yubico-piv-tool 2>/dev/null || { echo >&2 "I require yubico-piv-tool but it's not installed.  Aborting."; exit 1; }
 
 
-#2)	Check that a Yubikey NEO is connected
+#3)	Check that a Yubikey NEO is connected
 
 
 
-#3)	Prepare the Yubikey NEO
-#3a)	Generate a new token management key
+#4)	Prepare the Yubikey NEO
+#4a)	Generate a new token management key
 key=`dd if=/dev/random bs=1 count=24 2>/dev/null | hexdump -v -e '/1 "%02X"'`
 echo "Your new token management key is:"
 echo $key
-#3b)	Set the token management key
+#4b)	Set the token management key
 yubico-piv-tool -a set-mgm-key -n $key
