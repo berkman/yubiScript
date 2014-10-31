db411adf31a8f7ab6b77b0cfad40b25610dd7b8c
Mike Berkman <mike@berkman.co>
2014-10-31 10:06:41 -0400

    Generate a set a new management key

--- yubiScript.sh
+++ yubiScript.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-#  References:  http://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script/677212#677212
+#################################################
+#  References:  
+# -http://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script/677212#677212
+# - https://developers.yubico.com/yubico-piv-tool/YubiKey-NEO-PIV-Introduction.html
+#################################################
 
-# Check for the required binaries
+# Add the yubico-piv-tool binary to your path
+YUBI_BIN=$PWD/bin/
+export PATH=$PATH:${YUBI_BIN}
+
+#1)	Check for the required binaries
 hash openssl 2>/dev/null || { echo >&2 "I require openssl but it's not installed.  Aborting."; exit 1; }
 hash yubico-piv-tool 2>/dev/null || { echo >&2 "I require yubico-piv-tool but it's not installed.  Aborting."; exit 1; }
+
+
+#2)	Check that a Yubikey NEO is connected
+
+
+
+#3)	Prepare the Yubikey NEO
+#3a)	Generate a new token management key
+key=`dd if=/dev/random bs=1 count=24 2>/dev/null | hexdump -v -e '/1 "%02X"'`
+echo "Your new token management key is:"
+echo $key
+#3b)	Set the token management key
+yubico-piv-tool -a set-mgm-key -n $key
